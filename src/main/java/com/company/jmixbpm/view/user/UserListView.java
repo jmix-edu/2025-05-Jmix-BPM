8f353c99c77d3b9e99f7e22507b40cdfc634bc78
vashenkoda <d.vaschenko@haulmont.net>
2025-05-12 17:39:19 +0300

    Initial commit

---
+++ src/main/java/com/company/jmixbpm/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.jmixbpm.view.user;
+
+import com.company.jmixbpm.entity.User;
+import com.company.jmixbpm.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
